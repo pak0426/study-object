605b6a2a09d53219b1db0d5716358e491ba73391
hmmini <pak4184@naver.com>
2025-03-26 23:31:24 +0900

    first commit

---
+++ src/main/java/io/studyobject/StudyObjectApplication.java
@@ -0,0 +1,13 @@
+package io.studyobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyObjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/studyobject/StudyObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.studyobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

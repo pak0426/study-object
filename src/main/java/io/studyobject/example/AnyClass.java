097f1b9c637c39ffdd5dfdd76f36fc240d1f472b
hmmini <pak4184@naver.com>
2025-04-08 21:00:00 +0900

    04. 자율적인 객체를 향해

---
+++ src/main/java/io/studyobject/example/AnyClass.java
@@ -0,0 +1,8 @@
+package io.studyobject.example;
+
+public class AnyClass {
+    void anyMethod(Rectangle rectangle, int multiple) {
+        rectangle.setRight(rectangle.getRight() * multiple);
+        rectangle.setBottom(rectangle.getBottom() * multiple);
+    }
+}

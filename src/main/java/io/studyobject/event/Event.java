846b989fb12e2ec64a99b2bf7787ca3ad0b43816
hmmini <pak4184@naver.com>
2025-04-30 21:00:00 +0900

    명령과 쿼리를 함께 사용하면 버그를 찾기 어렵다.

--- src/main/java/io/studyobject/event/Event.java
+++ src/main/java/io/studyobject/event/Event.java
@@ -18,14 +18,13 @@ public class Event {
         if (from.getDayOfWeek() != schedule.getDayOfWeek() ||
             !from.toLocalTime().equals(schedule.getFrom()) ||
             !duration.equals(schedule.getDuration())) {
-            reschedule(schedule);
             return false;
         }
 
         return true;
     }
 
-    private void reschedule(RecurringSchedule schedule) {
+    public void reschedule(RecurringSchedule schedule) {
         from = LocalDateTime.of(from.toLocalDate().plusDays(daysDistance(schedule)),
                 schedule.getFrom());
         duration = schedule.getDuration();

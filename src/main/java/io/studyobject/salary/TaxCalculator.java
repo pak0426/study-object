65eadfa38b182bc52aee12939981e606c5978c3d
hmmini <pak4184@naver.com>
2025-05-06 17:10:16 +0900

    정보 은닉과 모듈

--- src/main/java/io/studyobject/salary/TaxCalculator.java
+++ src/main/java/io/studyobject/salary/TaxCalculator.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 
 public class TaxCalculator {
 
-    private static final String[] employees = {"직원A", "직원B", "직원C"};
-    private static final int[] basePays = {400, 300, 250};
-
     public static void main(String[] args) {
         String name = typingName();
         double taxRate = getTaxRate();
-        double pay = calculatePayFor(name, taxRate);
+        double pay = Employees.calculatePay(name, taxRate);
         System.out.println((describeResult(name, pay)));
     }
 
@@ -25,21 +22,6 @@ public class TaxCalculator {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().trim();
         return Double.parseDouble(input);
-    }
-
-    private static double calculatePayFor(String name, double taxRate) {
-        int index = findEmployeeIndex(name);
-        int basePay = basePays[index];
-        return basePay - (basePay * taxRate);
-    }
-
-    private static int findEmployeeIndex(String name) {
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].equals(name)) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     private static String describeResult(String name, double taxRate) {

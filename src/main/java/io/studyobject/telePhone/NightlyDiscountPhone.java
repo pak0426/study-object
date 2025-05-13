f9a00d2362804117ae4aad65fc2da5483f4dae1f
hmmini <pak4184@naver.com>
2025-05-13 21:00:00 +0900

    전화 요금을 계산하는 애플리케이션을 개발 - 심야요금제 추가 (중복 코드를 일부로 발생)

---
+++ src/main/java/io/studyobject/telePhone/NightlyDiscountPhone.java
@@ -0,0 +1,42 @@
+package io.studyobject.telePhone;
+
+import io.studyobject.movie.Money;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class NightlyDiscountPhone {
+    private static final int LATE_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    public void call(Call call) {
+        calls.add(call);
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+        if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+            result = result.plus(nightlyAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
+        } else {
+            result = result.plus(regularAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds()));
+        }
+            }
+
+        return result;
+    }
+}

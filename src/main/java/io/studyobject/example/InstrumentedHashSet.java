4946ba04d2460fa3166438b4d2dc60ab1c10fbaf
hmmini <pak4184@naver.com>
2025-05-18 21:00:00 +0900

    메서드 오버라이딩의 오작용 문제

---
+++ src/main/java/io/studyobject/example/InstrumentedHashSet.java
@@ -0,0 +1,20 @@
+package io.studyobject.example;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class InstrumentedHashSet<E> extends HashSet<E> {
+    private int addCount = 0;
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+}

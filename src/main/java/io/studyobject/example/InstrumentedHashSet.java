b60d4608aee0f1b36a0b37a35280af178fe7fcfe
hmmini <pak4184@naver.com>
2025-05-20 21:00:00 +0900

    메서드 오버라이딩의 오작용 문제 - 문제가 발생하는 코드 해결하는 방법

--- src/main/java/io/studyobject/example/InstrumentedHashSet.java
+++ src/main/java/io/studyobject/example/InstrumentedHashSet.java
@@ -15,12 +15,18 @@ public class InstrumentedHashSet<E> extends HashSet<E> {
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        addCount += c.size();
-        return super.addAll(c);
+        boolean modified = false;
+        for (E e : c) {
+            if (add(e)) {
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     public static void main(String[] args) {
         InstrumentedHashSet<String> languages = new InstrumentedHashSet<>();
         languages.addAll(Arrays.asList("Java", "Ruby", "Scala"));
+        System.out.println("languages.size() = " + languages.size());
     }
 }

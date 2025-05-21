0285a17b4479fae1343722e588a34b1f3c3e53a6
hmmini <pak4184@naver.com>
2025-05-21 21:00:00 +0900

    부모 클래스와 자식 클래스의 동시 수정 문제 - Song 클래스

---
+++ src/main/java/io/studyobject/example/Song.java
@@ -0,0 +1,11 @@
+package io.studyobject.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Song {
+    private String singer;
+    private String title;
+}

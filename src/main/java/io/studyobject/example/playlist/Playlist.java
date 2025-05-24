26434e3fd23f84a81ec40d52b546b11234dfb0b4
hmmini <pak4184@naver.com>
2025-05-24 21:00:00 +0900

    요구사항 변경 - 가수별 노래의 제목도 함께 관리

--- src/main/java/io/studyobject/example/playlist/Playlist.java
+++ src/main/java/io/studyobject/example/playlist/Playlist.java
@@ -3,13 +3,17 @@ package io.studyobject.example.playlist;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 public class Playlist {
     private List<Song> tracks = new ArrayList<>();
+    private Map<String, String> singers = new HashMap<>();
 
     public void append(Song song) {
-        getTracks().add(song);
+        tracks.add(song);
+        singers.put(song.getSinger(), song.getTitle());
     }
 }

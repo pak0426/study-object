0dbd6eeaa3b5148dc23f3097d9b884d68244d442
hmmini <pak4184@naver.com>
2025-05-27 21:00:00 +0900

    추상화에 의존하라 - NightlyDiscountPrice 상속 제거

--- src/main/java/io/studyobject/telePhone/NightlyDiscountPhone.java
+++ src/main/java/io/studyobject/telePhone/NightlyDiscountPhone.java
@@ -8,36 +8,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class NightlyDiscountPhone extends Phone {
+public class NightlyDiscountPhone {
     private static final int LATE_NIGHT_HOUR = 22;
 
     private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
     private List<Call> calls = new ArrayList<>();
 
-    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, double taxRate) {
-        super(regularAmount, seconds, taxRate);
+    public NightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds) {
         this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
     }
 
     public void call(Call call) {
         calls.add(call);
     }
 
-    @Override
     public Money calculateFee() {
-        Money result = super.calculateFee();
+        Money result = Money.ZERO;
 
-        Money nightlyFee = Money.ZERO;
         for (Call call : calls) {
             if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-                nightlyFee = nightlyFee.plus(
-                        getAmount().minus(nightlyAmount).times(
-                                (double) call.getDuration().getSeconds() / getSeconds().getSeconds()
-                        )
+                result = result.plus(
+                        nightlyAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds())
                 );
+            } else {
+                result = regularAmount.times((double) call.getDuration().getSeconds() / seconds.getSeconds());
             }
         }
 
-        return result.minus(nightlyFee.plus(nightlyFee.times(getTaxRate())));
+        return result;
     }
 }

e06beecb66dbe04e9786d0a82dcc45da6ca0c623
hmmini <pak4184@naver.com>
2025-06-12 21:00:00 +0900

    합성 - 기본 정책에 기본 요금 할인 정책 조합하기

---
+++ src/main/java/io/studyobject/telePhone/RateDiscountableNightlyDiscountPhone.java
@@ -0,0 +1,21 @@
+package io.studyobject.telePhone;
+
+import io.studyobject.movie.Money;
+
+import java.time.Duration;
+
+public class RateDiscountableNightlyDiscountPhone extends NightlyDiscountPhone {
+
+    private Money discountAmount;
+
+    public RateDiscountableNightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds, Money discountAmount) {
+        super(nightlyAmount, regularAmount, seconds);
+        this.discountAmount = discountAmount;
+    }
+
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.minus(discountAmount);
+    }
+}
---
+++ src/main/java/io/studyobject/telePhone/RateDiscountableRegularPhone.java
@@ -0,0 +1,20 @@
+package io.studyobject.telePhone;
+
+import io.studyobject.movie.Money;
+
+import java.time.Duration;
+
+public class RateDiscountableRegularPhone extends RegularPhone {
+
+    private Money discountAmount;
+
+    public RateDiscountableRegularPhone(Money amount, Duration seconds, Money discountAmount) {
+        super(amount, seconds);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.minus(discountAmount);
+    }
+}

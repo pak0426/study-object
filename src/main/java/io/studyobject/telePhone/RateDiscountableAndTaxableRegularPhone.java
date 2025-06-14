e16fb643741f065548942c227b482a9a7f9f16ec
hmmini <pak4184@naver.com>
2025-06-14 21:00:00 +0900

    합성 - 기본 할인 정책 부과 후 세금 정책 부과 클래스

---
+++ src/main/java/io/studyobject/telePhone/RateDiscountableAndTaxableRegularPhone.java
@@ -0,0 +1,19 @@
+package io.studyobject.telePhone;
+
+import io.studyobject.movie.Money;
+
+import java.time.Duration;
+
+public class RateDiscountableAndTaxableRegularPhone extends RateDiscountableRegularPhone {
+    private double taxRate;
+
+    public RateDiscountableAndTaxableRegularPhone(Money amount, Duration seconds, Money discountAmount, double taxRate) {
+        super(amount, seconds, discountAmount);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return super.afterCalculated(fee).plus(fee.times(taxRate));
+    }
+}

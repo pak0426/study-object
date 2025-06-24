040b9de765e39a451c24c89b64c101693a96fc19
hmmini <pak4184@naver.com>
2025-06-24 21:00:00 +0900

    합성 - 세금 할인 정책 구현

---
+++ src/main/java/io/studyobject/telePhone/composition/TaxablePolicy.java
@@ -0,0 +1,17 @@
+package io.studyobject.telePhone.composition;
+
+import io.studyobject.movie.Money;
+
+public class TaxablePolicy extends AdditionalRatePolicy {
+    private double taxRatio;
+
+    public TaxablePolicy(RatePolicy next, double taxRatio) {
+        super(next);
+        this.taxRatio = taxRatio;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.plus(fee.times(taxRatio));
+    }
+}

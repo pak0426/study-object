21bee2f2815032508f5dbf369bd8caf1d65e9017
hmmini <pak4184@naver.com>
2025-07-05 18:00:00 +0900

    서브클래싱과 서브타이핑 - 상속 관계를 유지하면서 문제를 해결하는 세 가지 방법

--- src/main/java/io/studyobject/example/is_a/Penguin.java
+++ src/main/java/io/studyobject/example/is_a/Penguin.java
@@ -1,4 +1,10 @@
 package io.studyobject.example.is_a;
 
 public class Penguin extends Bird {
+    @Override
+    public void fly() {
+    }
+
+    @Override
+    public void flyBird(Bird bird) {}
 }

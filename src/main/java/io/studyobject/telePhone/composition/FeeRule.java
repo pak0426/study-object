bf6a25a49e236a6f9f1dc6d33f9baaf89be840be
hmmini <pak4184@naver.com>
2025-07-23 18:00:00 +0900

    일관성 있는 협력 - 추상화 수준에서 협력 패턴 구현하기

---
+++ src/main/java/io/studyobject/telePhone/composition/FeeRule.java
@@ -0,0 +1,21 @@
+package io.studyobject.telePhone.composition;
+
+import io.studyobject.movie.Money;
+import io.studyobject.telePhone.Call;
+
+public class FeeRule {
+    private FeeCondition feeCondition;
+    private FeePerDuration feePerDuration;
+
+    public FeeRule(FeeCondition feeCondition, FeePerDuration feePerDuration) {
+        this.feeCondition = feeCondition;
+        this.feePerDuration = feePerDuration;
+    }
+
+    public Money calculateFee(Call call) {
+        return feeCondition.findTimeIntervals(call)
+                .stream()
+                .map(condition -> feePerDuration.calculate(condition))
+                .reduce(Money.ZERO, Money::plus);
+    }
+}

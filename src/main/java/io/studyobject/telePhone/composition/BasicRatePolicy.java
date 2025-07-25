0b94f3dd8b6251541357625f2b0803b626b3c2ce
hmmini <pak4184@naver.com>
2025-07-25 18:00:00 +0900

    일관성 있는 협력 - 추상화 수준에서 협력 패턴 구현하기

--- src/main/java/io/studyobject/telePhone/composition/BasicRatePolicy.java
+++ src/main/java/io/studyobject/telePhone/composition/BasicRatePolicy.java
@@ -3,16 +3,28 @@ package io.studyobject.telePhone.composition;
 import io.studyobject.movie.Money;
 import io.studyobject.telePhone.Call;
 
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class BasicRatePolicy implements RatePolicy {
+    private List<FeeRule> feeRules;
+
+    public BasicRatePolicy(FeeRule... feeRules) {
+        this.feeRules = Arrays.asList(feeRules);
+    }
+
     @Override
-    public Money calculateFee(CompositionPhone compositionPhone) {
-        Money result = Money.ZERO;
+    public Money calculateFee(CompositionPhone phone) {
+        return phone.getCalls()
+                .stream()
+                .map(call -> calculate(call))
+                .reduce(Money.ZERO, Money::plus);
+    }
 
-        for (Call call : compositionPhone.getCalls()) {
-            result.plus(calculateCallFee(call));
-        }
-
-        return result;
+    private Money calculate(Call call) {
+        return feeRules.stream()
+                .map(rule -> rule.calculateFee(call))
+                .reduce(Money.ZERO, Money::plus);
     }
 
     protected abstract Money calculateCallFee(Call call);

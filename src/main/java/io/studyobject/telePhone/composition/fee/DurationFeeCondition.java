85df31e6e60c63eaa1a2e78564c7a3160cd1efdb
hmmini <pak4184@naver.com>
2025-07-29 18:00:00 +0900

    일관성 있는 협력 - 구체적인 협력 구현하기 - DurationFeeCondition

--- src/main/java/io/studyobject/telePhone/composition/fee/DurationFeeCondition.java
+++ src/main/java/io/studyobject/telePhone/composition/fee/DurationFeeCondition.java
@@ -1,4 +1,33 @@
 package io.studyobject.telePhone.composition.fee;
 
-public class DurationFeeCondition {
+import io.studyobject.telePhone.Call;
+import io.studyobject.telePhone.composition.DateTimeInterval;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+
+public class DurationFeeCondition implements FeeCondition {
+
+    private Duration from;
+    private Duration to;
+
+    public DurationFeeCondition(Duration from, Duration to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public List<DateTimeInterval> findTimeIntervals(Call call) {
+        if (call.getInterval().duration().compareTo(from) < 0) {
+            return Collections.emptyList();
+        }
+
+        return List.of(DateTimeInterval.of(
+                call.getInterval().getFrom().plus(from),
+                call.getInterval().duration().compareTo(to) > 0 ?
+                        call.getInterval().getFrom().plus(to) :
+                        call.getInterval().getTo()
+        ));
+    }
 }

aebefe8ea4c3829d8bb0458c2953880c4a7dd402
hmmini <pak4184@naver.com>
2025-08-04 18:00:00 +0900

    부록 A 계약에 의한 설계 - 03. 계약에 의한 설계와 서브타이핑 - 계약 규칙

--- src/main/java/io/studyobject/telePhone/composition/AdditionalRatePolicy.java
+++ src/main/java/io/studyobject/telePhone/composition/AdditionalRatePolicy.java
@@ -1,6 +1,9 @@
 package io.studyobject.telePhone.composition;
 
 import io.studyobject.movie.Money;
+import io.studyobject.telePhone.Call;
+
+import java.util.List;
 
 public abstract class AdditionalRatePolicy implements RatePolicy {
     private RatePolicy next;
@@ -13,6 +16,18 @@ public abstract class AdditionalRatePolicy implements RatePolicy {
     public Money calculateFee(CompositionPhone compositionPhone) {
         Money fee = next.calculateFee(compositionPhone);
         return afterCalculated(fee);
+    }
+
+    @Override
+    public Money calculateFee(List<Call> calls) {
+        assert calls != null;
+
+        Money fee = next.calculateFee(calls);
+        Money result = afterCalculated(fee);
+
+        assert result.isGreaterThanOrEqual(Money.ZERO);
+
+        return result;
     }
 
     abstract protected Money afterCalculated(Money fee);

8e118feb4e5bb52fd0636c43a302941fbc9d45e2
hmmini <pak4184@naver.com>
2025-08-19 18:00:00 +0900

    부록 A 계약에 의한 설계 - 03. 계약에 의한 설계와 서브타이핑 - 가변성 규칙

---
+++ src/main/java/io/studyobject/example/appendix/a/Book.java
@@ -0,0 +1,9 @@
+package io.studyobject.example.appendix.a;
+
+public class Book {
+    private Publisher publisher;
+
+    public Book(Publisher publisher) {
+        this.publisher = publisher;
+    }
+}

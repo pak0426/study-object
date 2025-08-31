4a36ad1b01454e7979dc0954d1f7f0f9e57afa7c
hmmini <pak4184@naver.com>
2025-08-31 18:00:00 +0900

    부록 B 타입 계층의 구현 - 인터페이스를 사용한 구현

---
+++ src/main/java/io/studyobject/example/appendix/b/Sound.java
@@ -0,0 +1,13 @@
+package io.studyobject.example.appendix.b;
+
+public class Sound implements Effect {
+    @Override
+    public void activate() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}

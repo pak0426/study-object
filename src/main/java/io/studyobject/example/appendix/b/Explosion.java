60afb7c626d379a77705fd1ef995d388cf3df245
hmmini <pak4184@naver.com>
2025-09-01 18:00:00 +0900

    부록 B 타입 계층의 구현 - 인터페이스를 사용한 구현

---
+++ src/main/java/io/studyobject/example/appendix/b/Explosion.java
@@ -0,0 +1,23 @@
+package io.studyobject.example.appendix.b;
+
+public class Explosion implements Displayable, Effect {
+    @Override
+    public Point getPosition() {
+        return null;
+    }
+
+    @Override
+    public void update(Graphics graphics) {
+
+    }
+
+    @Override
+    public void activate() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
